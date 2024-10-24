fd9744e3133ef1721688f43341ddd2474c2e9800
TheOpti <wieckowski.piotr.93@gmail.com>
2024-10-24 22:40:42 +0200

    add tailwind and shadcnui component library

--- apps/chat-ui/src/App.tsx
+++ apps/chat-ui/src/App.tsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+
+import { Button } from "@/components/ui/button";
+
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -8,6 +11,7 @@ function App() {
 
   return (
     <>
+      <Button>This is shadcnui button</Button>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
