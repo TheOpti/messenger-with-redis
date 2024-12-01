63138b557c6a49dcfaf72abed81d9c810b00f096
TheOpti <wieckowski.piotr.93@gmail.com>
2024-12-01 23:03:58 +0100

    add postgresql connection to server app

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,10 +1,19 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
 import cors from "cors";
 import express from "express";
 import helmet from "helmet";
 import http from "http";
+import pg from "pg";
 import { Server } from "socket.io";
 
 import router from "./routers/authRouter";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const pool = new pg.Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -29,6 +38,8 @@ app.use("/auth", router);
 
 io.on("connect", (socket) => {});
 
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log(`Server is running on http://localhost:${port}`);
+  const result = await prisma.$connect();
+  console.log("Result: ", result);
 });
